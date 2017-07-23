da651f1cb5af4f3dcd37face196827253bb8ef25
Przemyslaw "Blasto" Wentrys <pwentrys@gmail.com>
2017-07-23 04:44:15 -0400

    Configuration + Credential examples.

---
+++ config/configuration_example.py
@@ -0,0 +1,9 @@
+from .credentials import PASS as PASSWORD
+
+
+PROTOCOL = 'mysql+pymysql'
+USERNAME = 'USERNAME'
+ADDRESS = 'ADDRESS'
+PORT = 3306
+SCHEMA = 'SCHEMA'
+OPTIONS = 'charset=utf8'
---
+++ config/credentials_example.py
@@ -0,0 +1,1 @@
+PASS = b'PASSWORD'
