1710aeb55e9c37c3ae1d4bee9a58811158d19b79
Przemyslaw "Blasto" Wentrys <pwentrys@gmail.com>
2017-07-23 07:36:28 -0400

    Auto Pep8.

--- generator/models/replace_flag.py
+++ generator/models/replace_flag.py
@@ -3,7 +3,8 @@ from utilities.typesext import Types as t
 
 class Replace_Flag:
     def __init__(self, target: str, destination='', flag=None):
-        assert isinstance(destination, t.StringType), f'Type is not string: {destination}'
+        assert isinstance(
+            destination, t.StringType), f'Type is not string: {destination}'
         if isinstance(flag, t.NoneType):
             flag = {self.target: True}
         assert isinstance(flag, t.DictType), f'Flag type is not dict: {flag}'
