a441526e3795154a34eba7388eebb4584bd730cf
Przemyslaw "Blasto" Wentrys <pwentrys@gmail.com>
2017-07-23 06:56:40 -0400

    Added/Improved comments on Types extension obj.

--- utilities/typesext.py
+++ utilities/typesext.py
@@ -7,19 +7,39 @@ class Types:
     StringType = type('')
 
     def add_type(self, key: str, c: type):
+        """
+        Add type to type dict.
+        :param key:
+        :param c:
+        :return:
+        """
         if not Types.__dict__.__contains__(key):
             Types.__setattr__(key, c)
 
     @staticmethod
     def from_key(key: str) -> type:
+        """
+        Return type from string.
+        :param key:
+        :return:
+        """
         assert key.endswith('Type'), f'{key} doesn\'t end with Type.'
         return Types._from_key(key)
 
     @staticmethod
     def _from_key(key: str) -> type:
+        """
+        Return int type from string.
+        :param key:
+        :return:
+        """
         return Types.__getattribute__(key)
 
     def __dolog__(self) -> str:
+        """
+        For debugging.
+        :return:
+        """
         out = self.__dict__.copy()
         out.update(Types.__dict__)
         return ''.join(
